2feb7d851322071db382b7fb1731357bd2225a06
Edson Galdino da Silva Alves <edson1galdino@gmail.com>
2022-10-08 22:58:17 -0300

    adicionando crossOrigin config

--- src/main/java/br/puc/edson/telepsicologiapsicologoservice/api/PsychologistController.java
+++ src/main/java/br/puc/edson/telepsicologiapsicologoservice/api/PsychologistController.java
@@ -6,10 +6,7 @@ import br.puc.edson.telepsicologiapsicologoservice.mapper.PsychologistMapper;
 import br.puc.edson.telepsicologiapsicologoservice.service.PsychologistService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,6 +14,7 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/psychologist")
 @AllArgsConstructor
+@CrossOrigin
 public class PsychologistController {
 
     private final PsychologistService service;
